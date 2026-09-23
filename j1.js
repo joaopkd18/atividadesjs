f4ff9667994ce99640e6a93c57760c56077d2dc1
João 𓆝 𓆟 𓆞 𓆝 <melojoaopaulo812@gmail.com>
2026-09-23 16:53:58 -0300

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,9 @@
+
+
+function  calcularAreaRetangulo() {
+    let base= Number(prompt("Digite o valor da base:"))
+    let altura= Number(prompt("Digite o valor da altura:"))
+    let area= base*altura
+    return alert ("A area do retangulo é: "+ area)
+}
+ calcularAreaRetangulo()
